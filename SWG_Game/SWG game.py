1473beb171a6cf05678700c44a110766653210a7
Sujal-Sawalkar <sujalsawalkar2005@gmail.com>
2025-07-02 00:24:44 +0530

    Update SWG game.py

--- SWG_Game/SWG game.py
+++ SWG_Game/SWG game.py
@@ -1,20 +1,29 @@
 import random as rd
-user = int(input("Enetr \n for Snake \n 2 for Water \n 3 for Gun \n Your choice:"))
-comp = rd.randint(1, 3)
 
 def check(comp, user):
     if(comp ==  user):
         print("Its a DRAW!")
-    elif(comp == 1 and user == 2):
+    elif(comp == "Snake" and user == "Water"):
         print("You LOSE!")
-    elif(comp == 3 and user == 1):
+    elif(comp == "Gun" and user == "Snake"):
         print("You LOSE!")
-    elif(comp == 2 and user == 3):
+    elif(comp == "Water" and user == "Gun"):
         print("You LOSE!")
     else:
         print("You WON!")
 
-check(comp, user)
-print("Your choice:", user)
-print("Computer's choice:", comp)
-    
+def exit_game():
+    print("👋 Thanks for playing! Goodbye.")
+    exit()
+
+
+while True:
+    options = ["Snake", "water","Gun"]
+    user = input("Enetr Snake, Water or Gun \n Your choice:")
+    comp = rd.choice(options)
+    check(comp, user)
+    print("Your choice:", user)
+    print("Computer's choice:", comp)
+    print("Enter \"quit\" or \" exit \" to exit the game")
+    if user.lower() in ["exit", "quit"]:
+         exit_game()
